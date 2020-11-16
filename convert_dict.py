ab45ace3ce9ea748c849a4e25fad39febf336470
Brighton Pauli <brightp@umich.edu>
2020-11-16 14:57:45 -0500

    Progress on Word Affirmation

--- convert_dict.py
+++ convert_dict.py
@@ -3,6 +3,56 @@ import json
 with open("cmu_dictionary.txt", 'r') as f:
     lines = f.read().split('\n')
 
+with open("symbol.json", 'r') as f:
+    SYMBOL = json.load(f)
+
+print(SYMBOL["nucleus"].items())
+
+def syllabalize(word):
+    
+    word = word.split(' ')
+    i = 0
+
+    while i < len(word):
+        a = {
+            "o": [],
+            "n": [],
+            "c": []
+        }
+
+        j = i
+
+        # print("+", ' '.join(a["n"] + [word[i]]))
+
+        while i < len(word) and ' '.join(a["o"] + [word[i]]) in SYMBOL["onset"].values():
+            a["o"].append(word[i])
+            i += 1
+        
+        while i < len(word) and ' '.join(a["n"] + [word[i]]) in SYMBOL["nucleus"].values():
+            a["n"].append(word[i])
+            i += 1
+        
+        while i < len(word) and ' '.join(a["c"] + [word[i]]) in SYMBOL["coda"].values():
+            a["c"].append(word[i])
+            i += 1
+        
+        if j == i:
+            print()
+            print(word)
+            print(word[i])
+            print(a)
+            break
+            
+        if not len(a["n"]):
+            if a["o"][-1] in "SLZRTD":
+                a["n"] = a["o"][-1]
+                del a["o"][-1]
+        
+        for p in a:
+            a[p] = ' '.join(a[p])
+        
+        yield a
+
 d = {}
 
 for line in lines:
@@ -10,14 +60,19 @@ for line in lines:
 
     a = a.split('(')[0]
     b = b.replace("ER", "R")
+    b = b.replace("DH", "TH")
+    b = b.replace("HH W", "W")
+    b = b.replace("L V TH", "L F TH")
+
+    for syl in syllabalize(b):
+        if syl["n"] == '':
+            print(b)
+            print(syl)
 
     if b not in d:
         d[b] = []
     
     d[b].append(a)
-
-    if len(d[b]) > 1:
-        print(d[b])
 
 with open("cmu.json", 'w') as f:
     json.dump(d, f)
